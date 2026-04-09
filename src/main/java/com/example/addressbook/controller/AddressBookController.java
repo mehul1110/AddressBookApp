729728297063eb432f1b4eed0be8717122494752
mehul1110 <mehulshra1110@gmail.com>
2026-04-09 16:10:23 +0530

    UC 7: Add Slf4j logging

--- src/main/java/com/example/addressbook/controller/AddressBookController.java
+++ src/main/java/com/example/addressbook/controller/AddressBookController.java
@@ -4,6 +4,7 @@ import com.example.addressbook.dto.AddressBookDTO;
 import com.example.addressbook.dto.ResponseDTO;
 import com.example.addressbook.model.AddressBookData;
 import com.example.addressbook.service.IAddressBookService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/addressbook")
+@Slf4j
 public class AddressBookController {
 
     @Autowired
@@ -20,6 +22,7 @@ public class AddressBookController {
 
     @GetMapping(value = {"", "/", "/get"})
     public ResponseEntity<ResponseDTO> getAddressBookData() {
+        log.info("Fetching all address book entries");
         List<AddressBookData> data = addressBookService.getAddressBookData();
         ResponseDTO dto = new ResponseDTO("Get Call Success", data);
         return new ResponseEntity<>(dto, HttpStatus.OK);
@@ -27,6 +30,7 @@ public class AddressBookController {
 
     @GetMapping("/get/{id}")
     public ResponseEntity<ResponseDTO> getAddressBookDataById(@PathVariable("id") int id) {
+        log.info("Fetching address book entry for id: {}", id);
         AddressBookData data = addressBookService.getAddressBookDataById(id);
         ResponseDTO dto = new ResponseDTO("Get Call Success for id", data);
         return new ResponseEntity<>(dto, HttpStatus.OK);
@@ -34,6 +38,7 @@ public class AddressBookController {
 
     @PostMapping("/create")
     public ResponseEntity<ResponseDTO> addAddressBookData(@RequestBody AddressBookDTO addressBookDTO) {
+        log.info("Creating new address book entry");
         AddressBookData data = addressBookService.createAddressBookData(addressBookDTO);
         ResponseDTO dto = new ResponseDTO("Created Address Book Data", data);
         return new ResponseEntity<>(dto, HttpStatus.OK);
@@ -41,6 +46,7 @@ public class AddressBookController {
 
     @PutMapping("/update/{id}")
     public ResponseEntity<ResponseDTO> updateAddressBookData(@PathVariable("id") int id, @RequestBody AddressBookDTO addressBookDTO) {
+        log.info("Updating address book entry for id: {}", id);
         AddressBookData data = addressBookService.updateAddressBookData(id, addressBookDTO);
         ResponseDTO dto = new ResponseDTO("Updated Address Book Data for id", data);
         return new ResponseEntity<>(dto, HttpStatus.OK);
@@ -48,6 +54,7 @@ public class AddressBookController {
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<ResponseDTO> deleteAddressBookData(@PathVariable("id") int id) {
+        log.info("Deleting address book entry for id: {}", id);
         addressBookService.deleteAddressBookData(id);
         ResponseDTO dto = new ResponseDTO("Deleted Address Book Data for id: " + id, null);
         return new ResponseEntity<>(dto, HttpStatus.OK);
