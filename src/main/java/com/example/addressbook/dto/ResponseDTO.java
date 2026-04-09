6af1db6a369e27f4bb0c5894f50f5d0ca9b7b51b
mehul1110 <mehulshra1110@gmail.com>
2026-04-09 16:09:01 +0530

    UC 6: Refactor using Lombok

--- src/main/java/com/example/addressbook/dto/ResponseDTO.java
+++ src/main/java/com/example/addressbook/dto/ResponseDTO.java
@@ -1,10 +1,13 @@
 package com.example.addressbook.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ResponseDTO {
     public String message;
     public Object data;
-    public ResponseDTO(String message, Object data) {
-        this.message = message;
-        this.data = data;
-    }
 }
